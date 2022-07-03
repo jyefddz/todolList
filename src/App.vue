6ab1542f5141bae7379e1be48e41f42beb9a5bb1
jyefddz <1065697146@qq.com>
2022-07-03 21:25:27 +0800

    todolist简单功能实现

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,80 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="todoapp">
+    <TodoHeader @add="addFn"></TodoHeader>
+    <TodoMain :list="showList" @del="delFn"></TodoMain>
+    <TodoFooter :count="count" @filterdata="filterdataFn" @clear="clearFn"></TodoFooter>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TodoHeader from "./components/TodoHeader";
+import TodoMain from "./components/TodoMain";
+import TodoFooter from "./components/TodoFooter";
 
 export default {
-  name: 'App',
+  data() {
+    return {
+      // list: [
+      //   { id: 100, name: "吃饭", isDone: true },
+      //   { id: 101, name: "睡觉", isDone: true },
+      //   { id: 102, name: "打豆豆", isDone: false },
+      //   { id: 103, name: "小樊按摩", isDone: true },
+      // ],
+      list: JSON.parse(localStorage.getItem("list")) || [],
+      getSel: 'all'
+    }
+  },
   components: {
-    HelloWorld
+    TodoHeader,
+    TodoFooter,
+    TodoMain
+  },
+  computed: {
+    showList() {
+      if (this.getSel == 'no') {
+        return this.list.filter((ele) => !ele.isDone)
+      }
+      else if (this.getSel == 'yes') {
+        return this.list.filter((ele) => ele.isDone)
+      }
+      else {
+        return this.list
+      }
+    },
+    count() {
+      return this.list.filter((ele) => !ele.isDone).length
+    }
+  },
+  methods: {
+    addFn(val) {
+      const id = this.list[this.list.length - 1] ? this.list[this.list.length - 1].id + 1 : 100
+      this.list.push({
+        id,
+        name: val,
+        isDone: false,
+      })
+    },
+    delFn(id) {
+      const index = this.list.findIndex((ele) => ele.id == id)
+      this.list.splice(index, 1)
+    },
+    filterdataFn(val) {
+      this.getSel = val
+    },
+    clearFn() {
+      this.list.forEach((ele) => (ele.isDone = false))
+    }
+  },
+  watch: {
+    list: {
+      deep: true,
+      handler(val) {
+        localStorage.setItem('list', JSON.stringify(val || []))
+      }
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/components/TodoHeader.vue
@@ -0,0 +1,39 @@
+<template>
+    <header class="header">
+        <h1>todos</h1>
+        <input id="toggle-all" class="toggle-all" type="checkbox" v-model="isAll">
+        <label for="toggle-all"></label>
+        <!-- label 可以关联一个表单标签 -->
+        <input class="new-todo" placeholder="输入任务名称-回车确认" autofocus v-model.trim="task" @keydown.enter="enter" />
+    </header>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            task: ''
+        }
+    },
+    methods: {
+        enter() {
+            if (this.task.length == 0) {
+                return alert('please enter a task')
+            }
+            this.$emit('add', this.task)
+            this.task = ''
+        }
+    },
+    computed: {
+        isAll: {
+            set(checked) {
+                this.$parent.list.forEach((ele) => (ele.isDone = checked))
+            },
+            get() {
+                return this.$parent.list.every((ele) => (ele.isDone))
+            }
+        }
+    }
+
+}
+</script>
